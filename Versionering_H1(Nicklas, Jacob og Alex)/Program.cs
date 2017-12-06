fbf859ebccfd62fb34cb14fcb6bca0ccaad35ab8
Nicklas <nicklasjensen@github.com>
2017-12-06 13:57:33 +0100

    Projeckt start
    starting the projeckt :)

---
+++ Versionering_H1(Nicklas, Jacob og Alex)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Versionering_H1_Nicklas__Jacob_og_Alex_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
